0f9229f008fa41a920edc0bfa08d251ed47b9443
Andrés Fernández <lafvg12@hotmail.com>
2022-04-12 01:11:31 -0500

    page 404

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("container"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets5.lottiefiles.com/packages/lf20_afwjhfb2.json", // lottie file path
+});
